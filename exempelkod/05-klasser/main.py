84172c2c5fef8d202c5a4ec7e2710d0bf4337493
hemphen <michael.hemph@tabyenskilda.se>
2025-02-06 08:09:42 +0100

    Methods are functions

--- exempelkod/05-klasser/main.py
+++ exempelkod/05-klasser/main.py
@@ -7,6 +7,12 @@ class Player:
         self.y = 0        # attribut: y-koordinat
         self.score = 0    # attribut: poäng
 
-# Skapa en instance (ett objekt) av klassen Player
+    # En metod som flyttar spelare en givet antal steg
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+# Skapa en spelare och flytta hen
 player1 = Player("Micke")
+player1.move(5, -2) # Förflyttar 5 steg i x och -2 steg i y
 print(f"{player1.name} har position ({player1.x}, {player1.y}) och poäng {player1.score}.")
