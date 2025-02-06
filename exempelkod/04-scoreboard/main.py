6fc0309b66759dcc2c74e9ff214fc5f58afb57e4
hemphen <michael.hemph@tabyenskilda.se>
2025-02-06 07:41:58 +0100

    Add scoreboard using font to example

--- exempelkod/04-scoreboard/main.py
+++ exempelkod/04-scoreboard/main.py
@@ -217,10 +217,17 @@ def main():
     Motsvarar en spelomgång i vårt spel.
     '''
 
+    # Intialisera pygame och font-modulen
+    pygame.init()
+    pygame.font.init()
+
     # Skapa en skärm att rita på och en klocka som
     # håller reda på tiden och våra frames
     screen = pygame.display.set_mode((WIDTH,HEIGHT))
     clock = pygame.time.Clock()
+
+    # Välj en standardfont
+    font = pygame.font.Font(None, 36)
 
     # Initialisera spelvärlden
     player = Player()
@@ -272,6 +279,11 @@ def main():
         for candy in candies:
             candy.draw(screen)
         player.draw(screen)
+
+        # Rita ut poängen på skärmen
+        text = f"Score: {player.points}"
+        text_surface = font.render(text, True, "white")  
+        screen.blit(text_surface, (10, 10))  # Top-left corner        
        
         # Flippa skärmen
         pygame.display.flip()
