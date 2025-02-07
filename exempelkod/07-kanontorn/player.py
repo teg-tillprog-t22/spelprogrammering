fc09eb715acfa651c22d9d1cc3a2c32e7196e8ca
hemphen <michael.hemph@tabyenskilda.se>
2025-02-07 14:55:57 +0100

    Add bullets to example 07

--- exempelkod/07-kanontorn/player.py
+++ exempelkod/07-kanontorn/player.py
@@ -1,5 +1,33 @@
 import pygame
 from math import pi
+
+class Bullet:
+    ALIVE = 1
+    DEAD = 0
+
+    def __init__(self, position, angle):
+        self.angle = angle
+        self.speed = 5
+        self.size = 4
+        self.velocity = pygame.Vector2.from_polar((self.speed, self.angle))
+        self.position = position + self.velocity.normalize()*70
+        self.state = Bullet.ALIVE
+
+    def update(self):
+        self.position += self.velocity
+        if self.position.x<0 or self.position.x>800 or self.position.y<0 or self.position.y>600:
+            self.die()
+
+    def die(self):
+        self.state = Bullet.DEAD
+
+    def is_alive(self):
+        return self.state==Bullet.ALIVE
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.size)
+
+
 class Player:
     '''
     Representerar spelaren
@@ -16,17 +44,31 @@ class Player:
         self.points = 0
         self.angle = 200
         self.speed = 180/50
+        self.bullets = []
+        self.fire_delay = 100
+        self.last_shot = 0
 
     def update(self, keys):
         '''
         Uppdatera spelarens position och tillstånd
         Reagera på knapptryckningar
-        '''
+        '''        
         self.angle += self.speed
         if self.angle>350 or self.angle<190:
             self.speed = -self.speed
             self.angle += self.speed   
+        
+        for bullet in self.bullets:
+            bullet.update()
 
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_shot > self.fire_delay:
+            self.last_shot = now
+            self.bullets.append(Bullet(self.position, self.angle))
 
     def draw(self, screen):
         '''
@@ -42,6 +84,10 @@ class Player:
             self.position + pygame.Vector2(70,5).rotate(self.angle)
         ]
         pygame.draw.polygon(screen, "blue", points)
+
+        self.bullets = [bullet for bullet in self.bullets if bullet.is_alive()]
+        for bullet in self.bullets:
+            bullet.draw(screen)
     
     
     def get_rect(self):
