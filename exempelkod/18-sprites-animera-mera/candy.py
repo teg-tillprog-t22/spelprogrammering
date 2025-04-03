0eec8b4f6650c23371cb2192588467e5de1b9b16
hemphen <michael.hemph@tabyenskilda.se>
2025-04-03 08:11:40 +0200

    Add example 18

---
+++ exempelkod/18-sprites-animera-mera/candy.py
@@ -0,0 +1,27 @@
+import pygame
+import random
+
+# Definiera bilden som en
+COIN_IMAGE = pygame.image.load("coin.png")
+
+class Candy(pygame.sprite.Sprite):
+    def __init__(self, area):
+        super().__init__()
+        # Area representerar skärmens storlek
+        self.area = area
+        self.image = COIN_IMAGE
+        self.rect = self.image.get_rect()
+        self.speed = 3
+        # Slumpa placeringen av myntet
+        x = random.randint(area.left, area.right - self.rect.width)
+        y = random.randint(area.top, area.bottom - self.rect.height)
+        # Sätt positionen på rektangeln själv
+        self.rect.topleft = (x, y)
+        self.created_at = pygame.time.get_ticks()
+
+    def update(self):
+        now = pygame.time.get_ticks()
+        # Godiset försvinner genom att döda sig själv efter 5 sekunder
+        if now - self.created_at > 2000:
+            self.kill()
+
